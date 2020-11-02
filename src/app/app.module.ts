6bd39e56d24dd8f4e34b18aadb02e865ebbf8fe5
hassansribet <sribethassan@gmail.com>
2020-11-02 22:22:54 +0100

    create navBar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './_layouts/nav-bar/nav-bar.component';
+import { BoardComponent } from './_pages/projects/board/board.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    BoardComponent
   ],
   imports: [
     BrowserModule
